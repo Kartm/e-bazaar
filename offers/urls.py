163240f07dc6e7b6bf9366b30e27d6f30251634c
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-04-13 06:00:03 +0200

    Add placeholder for offer details view

--- offers/urls.py
+++ offers/urls.py
@@ -5,4 +5,5 @@ app_name = "offers"
 
 urlpatterns = [
     path("", views.offers_feed_view, name="offers_feed_view"),
+    path("offers/<int:pk>", views.offer_details_view, name="offer_details_view"),
 ]
--- offers/views.py
+++ offers/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def offers_feed_view(request):
     return HttpResponse(f"todo: show feed")
+
+
+def offer_details_view(request, pk):
+    return HttpResponse(f"todo: show offer details")
