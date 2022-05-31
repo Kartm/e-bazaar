d643ae13ba639aa224ed21fc724e1e7824c13818
Marcin Kasperski <marcin.kspr@gmail.com>
2022-05-31 17:56:00 +0200

    removed the messages for successful login, successful register, logout

--- users/views.py
+++ users/views.py
@@ -16,7 +16,7 @@ def register_request(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, "Registration successful.")
+            # messages.success(request, "Registration successful.")
             return redirect(".")
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
@@ -32,7 +32,7 @@ def login_request(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
+                # messages.info(request, f"You are now logged in as {username}.")
                 return redirect(".")
             else:
                 messages.error(request, "Invalid username or password.")
@@ -61,7 +61,7 @@ def change_password_request(request):
 
 def logout_request(request):
     logout(request)
-    messages.info(request, "You have successfully logged out.")
+    # messages.info(request, "You have successfully logged out.")
     return redirect(".")
 
 
